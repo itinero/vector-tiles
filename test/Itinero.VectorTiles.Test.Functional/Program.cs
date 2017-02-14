40dc1a79adc27f1152ae89dbf4d9552c5a2f40f9
ben.abelshausen <ben@osmsharp.com>
2017-02-14 10:49:08 +0100

    Added nuspecs.

--- test/Itinero.VectorTiles.Test.Functional/Program.cs
+++ test/Itinero.VectorTiles.Test.Functional/Program.cs
@@ -14,23 +14,23 @@ namespace Itinero.VectorTiles.Test.Functional
             var routerDb = RouterDb.Deserialize(File.OpenRead(@"C:\work\data\routing\belgium.c.cf.routerdb"));
 
             var tile = Tiles.Tile.CreateAroundLocation(51.267966846313556f, 4.801913201808929f, 10);
-            var tileRange = tile.GetSubTiles(14);
+            var tileRange = tile.GetSubTiles(11);
 
             foreach (var t in tileRange)
             {
                 var segments = routerDb.ExtractTile(t.Id);
 
-                var tileGeoJson = Itinero.VectorTiles.GeoJson.GeoJsonTileWriter.ToGeoJson(segments, routerDb);
+                //var tileGeoJson = Itinero.VectorTiles.GeoJson.GeoJsonTileWriter.ToGeoJson(segments, routerDb);
 
-                using (var stream = File.Open("test.mvt", FileMode.Create))
+                using (var stream = File.Open(t.Id.ToInvariantString() + ".mvt", FileMode.Create))
                 {
                     Itinero.VectorTiles.Mapbox.MapboxTileWriter.Write(segments, t, routerDb, 4096, stream);
 
-                    stream.Flush();
-                    stream.Seek(0, SeekOrigin.Begin);
-                    var parsed = global::Mapbox.Vector.Tile.VectorTileParser.Parse(stream);
-                    var geoJson = parsed[0].ToGeoJSON(t.X, t.Y, t.Zoom);
-                    var json = Newtonsoft.Json.JsonConvert.SerializeObject(geoJson);
+                    //stream.Flush();
+                    //stream.Seek(0, SeekOrigin.Begin);
+                    //var parsed = global::Mapbox.Vector.Tile.VectorTileParser.Parse(stream);
+                    //var geoJson = parsed[0].ToGeoJSON(t.X, t.Y, t.Zoom);
+                    //var json = Newtonsoft.Json.JsonConvert.SerializeObject(geoJson);
 
                 }
             }
