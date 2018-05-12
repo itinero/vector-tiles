10979c1ef83a43e5f481f1612b32a09257ec2d3c
xivk <ben@itinero.tech>
2018-05-12 15:38:00 +0100

    Updated version #.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -22,5 +22,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.14")]
-[assembly: AssemblyInformationalVersion("0.0.14-alpha")]
+[assembly: AssemblyVersion("0.0.15")]
+[assembly: AssemblyInformationalVersion("0.0.15-alpha")]
