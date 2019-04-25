d997aa10ca63a94a02ae9fe66b14323fff087266
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-04-25 13:29:22 +0200

    Version bump

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.21")]
-[assembly: AssemblyInformationalVersion("0.0.21-alpha")]
+[assembly: AssemblyVersion("0.0.22")]
+[assembly: AssemblyInformationalVersion("0.0.22-alpha")]
